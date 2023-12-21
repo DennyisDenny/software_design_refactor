bade89fef87f6578e67da52f076c1db546e60a97
YU-LI CHENG <p76121314@gs.ncku.edu.tw>
2023-12-21 19:29:10 +0800

    Grnerate class Log for class ValidDatasetLog and class ValidationLog to inherit: For experimental logging

---
+++ trainer/Logger.py
@@ -0,0 +1,29 @@
+import os
+
+class Logger:
+    def __init__(self, experiment_name):
+        self.__experiment_name = experiment_name
+    
+    def write_log(self):
+        pass
+    
+class ValidDatasetLogger(Logger):
+    def __init__(self):
+        super().__init__(experiment_name)
+
+    def write_log(self, valid_dataset_log):
+        log = open(f'./saved_models/{self.__experiment_name}/log_dataset.txt', 'a', encoding="utf8")
+        log.write(valid_dataset_log)
+        print('-' * 80)
+        log.write('-' * 80 + '\n')
+        log.close()
+        
+class ValidationLogger(Logger):
+    def __init__(self, experiment_name):
+        super().__init__(experiment_name)
+        
+    def write_log(self, loss_model_log, predicted_result_log):
+        with open(f'./save_models/{self.__experiment_name}/log_train.txt', 'a', encoding='utf8') as log:
+            loss_model_log = f'{loss_log}\n{current_model_log}\n{best_model_log}'
+            log.write(loss_model_log + '\n')
+            log.write(predicted_result_log + '\n')
